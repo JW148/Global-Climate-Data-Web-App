0c68369655eef2354369d35ed960d2b43084e5aa
JW148 <47108195+JW148@users.noreply.github.com>
2023-09-10 19:35:28 +0100

    MaterialUI setup using slider component for date input

--- components/Cesium.js
+++ components/Cesium.js
@@ -1,13 +1,31 @@
-import { Cartesian3, Color, Ion } from "cesium";
-import { useState } from "react";
-import { Entity, Viewer } from "resium";
+import {
+  Cartesian3,
+  Color,
+  Ion,
+  JulianDate,
+  ClockRange,
+  ClockStep,
+  KeyboardEventModifier,
+  ScreenSpaceEventType,
+} from "cesium";
+import { useState, useRef, useEffect } from "react";
+import {
+  Clock,
+  Entity,
+  Viewer,
+  ScreenSpaceEventHandler,
+  ScreenSpaceEvent,
+} from "resium";
 
 import { API_KEY } from "../API";
 Ion.defaultAccessToken = API_KEY;
 
 export default function Cesium({ centroids }) {
+  const ref = useRef(null);
+  const [state, setState] = useState(null);
+
   return (
-    <Viewer full>
+    <Viewer full ref={ref} timeline={false} animation={false}>
       {centroids &&
         centroids.map((el) => {
           return (
@@ -21,6 +39,12 @@ export default function Cesium({ centroids }) {
             />
           );
         })}
+      <ScreenSpaceEventHandler>
+        <ScreenSpaceEvent
+          action={(evt) => console.log(evt)}
+          type={ScreenSpaceEventType.LEFT_CLICK}
+        />
+      </ScreenSpaceEventHandler>
     </Viewer>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,47 @@ import dynamic from "next/dynamic";
 import fs from "fs";
 import path from "path";
 
+//materialUI fonts
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { Slider } from "@mui/material";
+
 const Cesium = dynamic(() => import("../components/Cesium"), { ssr: false });
 
-export default function Home({ centroidData }) {
+export default function Home({ centroidData, cityTempData }) {
+  console.log(cityTempData[0]);
   return (
     <>
       <Head>
         <link rel="stylesheet" href="cesium/Widgets/widgets.css" />
       </Head>
       <Cesium centroids={centroidData} />
+      <Slider
+        aria-label="Temperature"
+        defaultValue={30}
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
     </>
   );
 }
 
-import { getCountryCentroids } from "../lib/dataParsing";
+import { getCountryCentroids, getDateRange } from "../lib/dataParsing";
 
 export async function getStaticProps() {
   const centroidData = getCountryCentroids();
+  const cityTempData = getDateRange();
 
   return {
     props: {
       centroidData,
+      cityTempData,
     },
   };
 }
